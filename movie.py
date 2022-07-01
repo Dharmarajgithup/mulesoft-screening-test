198bc68833303baddfe8025a65d4e84e67a4fa64
Dharmarajgithup <108517486+Dharmarajgithup@users.noreply.github.com>
2022-07-01 14:14:16 +0530

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,14 @@
+import sqlite3
+connection = sqlite3.connect('shows.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Movies
+              (MovieName TEXT,Actor TEXT,Actress TEXT, Year INT,Director TEXT)''')
+cursor.execute('''INSERT INTO Movies (MovieName,Actor,Actress,Year,Director) VALUES ('bigil', 'Vijay', 'Rashmika Mandanna', 2023,'Vamshi Paidipally' )''')
+cursor.execute('''INSERT INTO Movies (MovieName,Actor,Actress,Year,Director)VALUES ('Asuran', 'Dhanush', 'Manju Warrier', 2019,'Vetrimaran' )''')
+cursor.execute('''INSERT INTO Movies (MovieName,Actor,Actress,Year,Director)VALUES ('Ayan', 'Surya', 'Tamana Bhatia','2009,'K V Anand' )''')
+cursor.execute('''INSERT INTO Movies (MovieName,Actor,Actress,Year,Director)VALUES ('Beast', 'Vijay', 'Pooja Hegde', 2022,'Nelson' )''')
+cursor.execute('''SELECT * from Movies''')
+result = cursor.fetchall();
+print(result)
+connection.commit()
+connection.close()
